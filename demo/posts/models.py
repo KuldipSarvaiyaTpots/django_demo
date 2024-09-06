0c3503efaabff66f808a6483c0456acb9d95771d
KuldipSarvaiyaTpots <kuldip.sarvaiya@tpots.co>
2024-09-06 16:18:56 +0530

    added image support and name shown on admin panel

--- demo/posts/models.py
+++ demo/posts/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class posts(models.Model):
   title = models.CharField(max_length=255)
   content = models.TextField()
-  slug = models.SlugField()
+  slug = models.SlugField(blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+  banner = models.ImageField(default="fallback.png", blank=True)
+  
+  def __str__(self):
+    return self.title
